d3acac91dc6f60de7bba6b5843ec645233c74d2f
Julien LAMBY <jlamby@tp.michelin.com>
2015-02-09 15:17:56 +0100

    Adding Main class and bundle app with dependencies

---
+++ src/main/java/com/mtp/Main.java
@@ -0,0 +1,10 @@
+package com.mtp;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AdditionService additionService = new AdditionService();
+        System.out.println(additionService.add(2, 3));
+    }
+
+}
